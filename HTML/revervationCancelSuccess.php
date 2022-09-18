5642082b8ebd165e5f0c25170a2d64ee09459b30
kashpiya <notyourdope216@gmail.com>
2022-09-19 00:15:04 +0600

    reservationCancelSuccess.php is working

--- HTML/revervationCancelSuccess.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    echo "<h1> Successful Cancel </h1>"
-    ?>
-        <!-- php off then write other language then again php on -->
-        <!-- going to login page-->
-        <script>window.location.assign('reservation.php')</script>
-    <?php
-?>
